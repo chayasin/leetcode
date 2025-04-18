329b8e70ddb6723bfe0975613af8f6b6490ff4e6
Chayasin Saetia <chayasin.sa@bdi.or.th>
2025-04-19 00:13:51 +0700

    two sum

---
+++ two_sum.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+        d = dict(zip(nums, range(len(nums))))
+        print(d)
+        for i in range(len(nums)):
+            c = target - nums[i]
+            if c in d and d[c] != i:
+                return [i, d[c]]
+        return -1
