0df94468c5c6b4c1d85c4100ff74b7dac85db3e2
Chayasin Saetia <chayasin.sa@bdi.or.th>
2025-04-21 21:18:43 +0700

    search_insert_position

---
+++ easy/search_insert_position.py
@@ -0,0 +1,8 @@
+import List
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        for i in range(len(nums)):
+            if target <= nums[i]:
+                return i
+        return len(nums)
+        
